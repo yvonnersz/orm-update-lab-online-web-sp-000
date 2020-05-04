3fe46809c3204579624e2aeaaf5adc1b1f772d99
Yvonne M Pham <yvonnersz@gmail.com>
2020-05-04 23:38:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -50,10 +50,11 @@ class Student
   end
 
   def self.new_from_db(row)
-    student = self.new
-    student.id = row[0]
-    student.name = row[1]
-    student.grade = row[2]
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    student
   end
 
   def self.find_by_name(name)
