9c7f04fe1ca5b9b93c0978174467b8929b6fd553
Yvonne M Pham <yvonnersz@gmail.com>
2020-05-04 23:37:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,13 +1,6 @@
 class Student
   attr_accessor :name, :grade, :id
 
-  def self.new_from_db(row)
-    new_student = self.new
-    new_student.id = row[0]
-    new_student.name = row[1]
-    new_student.grade = row[2]
-  end
-
   def initialize(id=nil, name, grade)
     @name = name
     @grade = grade
@@ -56,7 +49,12 @@ class Student
     student
   end
 
-
+  def self.new_from_db(row)
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+  end
 
   def self.find_by_name(name)
     sql = <<-SQL
